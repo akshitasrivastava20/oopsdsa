978d08add7ccb965f298233d478cd0c4e5257090
Your Name <akshita.srivastava.1505@gmail.com>
2025-08-07 11:14:55 +0530

    oops

---
+++ collections/Main.java
@@ -0,0 +1,13 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list=new ArrayList<>();
+        List<Integer> vector=new Vector<>();
+        
+    }
+}
---
+++ enumeample/Basic.java
@@ -0,0 +1,14 @@
+package enumeample;
+
+public class Basic {
+    enum Week{
+        monday,tuesday,wednesday,thursday,friday,saturday,sunday
+        //these are constants
+        //public static and final
+        // can't be inherited
+    }
+    public static void main(String[] args) {
+        Week week=Week.monday;
+        
+    }
+}
